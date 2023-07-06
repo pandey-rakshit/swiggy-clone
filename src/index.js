0c878f45d50fdc2666b4a8e64f024623af9e6305
pandey-rakshit <pandeyrakshit03@gmail.com>
2023-07-06 12:40:42 +0530

    ADD: Footer

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import Navbar from "./Components/Navbar/Nav";
+import Footer from "./Components/Footer/Footer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Navbar />
     <App />
+    <Footer />
   </React.StrictMode>
 );
